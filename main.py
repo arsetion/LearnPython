c4e7dbdf18a53e8b45b2fb379ff06c11dfa9acc2
arsetion <@intel.com>
2018-08-06 16:04:09 -0700

    put it into a function and calling it from another script

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-import argparse
+import parser
 
-parser = argparse.ArgumentParser()
-parser.add_argument("name", help="your name")
-parser.add_argument("color", help="fave color")
-parser.add_argument("-v","--verbose", help="increase verbosity", action="count", default=0)
-args = parser.parse_args()
-if args.verbose >=2:
-    print("'{}' favority color is '{}'".format(args.name, args.color))
-elif args.verbose >=1:
-    print("'{}' likes '{}'".format(args.name, args.color))
-else:
-    print("'{}' '{}'".format(args.name, args.color))
+def main_func():
+    print ('main function')
+
+
+if __name__ == '__main__':
+    parser.parser_func()
+    main_func()
---
+++ parser.py
@@ -0,0 +1,19 @@
+import argparse
+
+def parser_func():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("name", help="your name")
+   parser.add_argument("color", help="fave color")
+   parser.add_argument("-v","--verbose", help="increase verbosity", action="count", default=0)
+   args = parser.parse_args()
+   if args.verbose >=2:
+       print("'{}' favority color is '{}'".format(args.name, args.color))
+   elif args.verbose >=1:
+       print("'{}' likes '{}'".format(args.name, args.color))
+   else:
+       print("'{}' '{}'".format(args.name, args.color))
+
+
+if __name__ == '__main__':
+    #parser.py is executed as a script
+    parser_func()
